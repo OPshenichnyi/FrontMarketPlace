c1fca85cd9050386d461f2dba5a9d0610551e6a3
OPshenichnyi <106489713+OPshenichnyi@users.noreply.github.com>
2024-02-20 19:00:58 +0200

    create new branch gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+function sum(a: number, b: number) {
+  return a + b;
+}
+
+console.log(sum(1, 2));
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
